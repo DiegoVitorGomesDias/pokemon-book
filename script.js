3783d2297c34fde9be7b44e71365ec4e4baa24bf
Diego V <diegovitorgomesdias@gmail.com>
2022-07-23 22:06:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+const form__pokemon = document.querySelector('.form');
+const pokemon__info = document.querySelector('.info__pokemon');
+const pokemon__id = document.querySelector('.id__pokemon');
+const pokemon__name = document.querySelector('.name__pokemon');
+const pokemon__image = document.querySelector('.image__pokemon');
+const input__pokemon = document.querySelector('.search__pokemon');
+const prev__pokemon = document.querySelector('.btn-left');
+const next__pokemon = document.querySelector('.btn-right');
+
+let id__pokemon = 1;
+
+const fecht__pokemon__api = async (name__pokemon = "erro") =>
+{
+    const respost = await fetch(`https://pokeapi.co/api/v2/pokemon/${name__pokemon}`);
+
+    if(respost.status == 200)
+    {
+        const pokemon_json = await respost.json();
+        return pokemon_json;
+    }
+
+    console.clear();
+    return "error";
+}
+
+const handle__pokemon = async (name__pokemon) =>
+{
+    pokemon__id.innerHTML = "";
+    pokemon__image.style.display = "none";
+    pokemon__name.innerHTML = "...";
+
+    name__pokemon = name__pokemon.toString().trim().replace(/^0+(?=\d)/, '').toLocaleLowerCase();
+    name__pokemon = name__pokemon == "" ? id__pokemon : name__pokemon;
+    const pokemon = await fecht__pokemon__api(name__pokemon);
+    
+    if(pokemon != "error")
+    {
+        id__pokemon = pokemon.id;
+        pokemon__id.innerHTML = pokemon.id;
+        pokemon__name.innerHTML = pokemon.name;
+        pokemon__image.style.display = "block";
+        
+        if(pokemon["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"])
+        {
+            pokemon__image.src = pokemon["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+        }
+        else if(pokemon["sprites"]["versions"]["generation-v"]["black-white"]["front_default"])
+        {
+            pokemon__image.src = pokemon["sprites"]["versions"]["generation-v"]["black-white"]["front_default"];
+        }
+        else
+        {
+            pokemon__image.style.display = "none";
+        }
+
+        if(pokemon__name.textContent.length > 19)
+        {
+            pokemon__info.style.fontSize = "clamp(0.1em, 4vw, 1.2em)";
+        }
+    }
+    else
+    {
+        pokemon__id.innerHTML = "";
+        pokemon__name.innerHTML = "não encontrado";
+        pokemon__image.style.display = "none";
+    }
+    
+    input__pokemon.value = "";
+    input__pokemon.focus();
+        
+}
+
+form__pokemon.addEventListener("submit", (e) =>
+{
+    e.preventDefault();
+    handle__pokemon(input__pokemon.value);
+})
+
+prev__pokemon.addEventListener("click", prev = () =>
+{
+    if(id__pokemon > 1)
+    {
+        id__pokemon --;
+        handle__pokemon(id__pokemon);
+    }
+})
+
+next__pokemon.addEventListener("click", next = () =>
+{
+    if(id__pokemon <= 905)
+    {
+        id__pokemon ++;
+        handle__pokemon(id__pokemon);
+    }
+})
+
+document.body.addEventListener("keydown", (e) =>
+{
+    /* console.log(e.code); */
+    if(document.activeElement.name != "search__pokemon" || document.activeElement.value == "")
+    {
+        switch (e.code) 
+        {
+            case "ArrowRight": next(); break;
+            case "ArrowLeft": prev(); break;
+            case "ArrowUp": next(); break;
+            case "ArrowDown": prev(); break;
+            case "Space": input__pokemon.focus(); break;
+        }
+    }
+    else
+    {
+        switch (e.code) 
+        {
+            case "ArrowUp": next(); break;
+            case "ArrowDown": prev(); break;
+        }
+    }
+})
+
+handle__pokemon(id__pokemon);
